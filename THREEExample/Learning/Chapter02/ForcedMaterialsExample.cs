c4283ee87717d91c778e85e3f2fa6b80262cc4da
smart <heonjoy@naver.com>
2022-04-15 21:27:22 +0900

    Initial Commit

---
+++ THREEExample/Learning/Chapter02/ForcedMaterialsExample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using THREE.Materials;
+using THREE.Math;
+
+namespace THREEExample.Learning.Chapter02
+{
+    [Example("03-ForcedMaterials", ExampleCategory.LearnThreeJS, "Chapter02")]
+    public class ForcedMaterialsExample : BasicSceneExample
+    {
+        public ForcedMaterialsExample() : base()
+        {
+            scene.OverrideMaterial = new MeshLambertMaterial() { Color = new Color().SetHex(0xffffff) };
+        }
+    }
+}
