888c87c8bed53dcef05584064892d6666a0b1b7d
smart <heonjoy@naver.com>
2024-07-18 21:25:09 +0900

    change DeepCopy with FastDeepCloner

--- THREE/Extensions/ThreeObjectExtension.cs
+++ THREE/Extensions/ThreeObjectExtension.cs
@@ -10,28 +10,8 @@ namespace THREE
     {
         public static T DeepCopy<T>(this T source) where T : new()
         {
-            if(!typeof(T).IsSerializable)
-            {
-                return source;
-            }
-            try
-            {
-                object result = null;
-                using(var ms = new MemoryStream())
-                {
-                    var formatter = new BinaryFormatter();
-                    formatter.Serialize(ms, source);
-                    ms.Position = 0;    
-                    result = (T)formatter.Deserialize(ms);
-                    ms.Close();
-                }
+            return (T)FastDeepCloner.DeepCloner.Clone(source);
 
-                return (T)result;
-            }
-            catch(Exception)
-            {
-                return new T();
-            }
         }
     }
 }
